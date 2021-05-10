f63278d711f82568617003ebdcd31a8992342180
Pedro Passos <73182976+pedro-22002182@users.noreply.github.com>
2021-05-10 16:10:47 +0100

    Construtor Peca

--- Projeto1/Peca.cs
+++ Projeto1/Peca.cs
@@ -1,12 +1,22 @@
 namespace Projeto1
 {
 
-    private int posX, posY;
-
-    private bool playerA; //se true então pertecen ao jogador A, se false pertence B
-
     public class Peca
     {
+        private int posX, posY;
+
+        private bool playerA; //se true então pertecen ao jogador A, se false pertence B
+
+        
+        public Peca(bool playerA)
+        {
+            //posicao inical da peca ("fora tabuleiro")
+            this.posX = 1;
+            this.posY = 5;
+
+            //pertence ao jogadorA?? B?
+            this.playerA = playerA;
+        }
         
     }
 }
