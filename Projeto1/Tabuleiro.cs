29561027bb0f1bcab83ae92d8544ba18fe137d39
Pedro Passos <73182976+pedro-22002182@users.noreply.github.com>
2021-05-10 15:50:14 +0100

    Update Tabuleiro
    criaçao array2D tabuleiro e  array pecas

--- Projeto1/Tabuleiro.cs
+++ Projeto1/Tabuleiro.cs
@@ -1,7 +1,18 @@
 namespace Projeto1
 {
+
+    private int[][] tabuleiro = new int[3][8];  
+
+    private Peca[] pecas = new Peca[14];
+
+
     public class Tabuleiro
     {
         
     }
+
+
+
+
+
 }
