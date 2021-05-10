8f2c11226d777d262c6582f8ca7deb19617643c7
Pedro Passos <73182976+pedro-22002182@users.noreply.github.com>
2021-05-10 16:35:20 +0100

    Funcao Check Flower (classe Tabuleiro)

--- Projeto1/Tabuleiro.cs
+++ Projeto1/Tabuleiro.cs
@@ -8,8 +8,8 @@ namespace Projeto1
 
         public Tabuleiro()
         {
-            this.tabuleiro = new int [3][8];
-            this.pecas = new Peca[14];
+            this.tabuleiro = new int [2][7];
+            this.pecas = new Peca[13];
 
             for(int i = 0; i < pecas.Length(); i++)
             {
@@ -25,6 +25,35 @@ namespace Projeto1
                 }
             }
         }
-    }
 
+
+        private bool CheckFlower(int x, int y)
+        {
+            if(x == 0 && y == 0)
+            {
+                return true;
+            }
+            else if(x == 2 && y == 0)
+            {
+                return true;
+            }
+            else if(x == 1 && y == 3)
+            {
+                return true;
+            }
+            else if(x == 0 && y == 6)
+            {
+                return true;
+            }
+            else if(x == 2 && y == 6)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+
+
+    }
 }
