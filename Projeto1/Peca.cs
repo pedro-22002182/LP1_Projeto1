03ab59eb47d45855d3ec6880ba9d9ad9a4830c08
Pedro Passos <73182976+pedro-22002182@users.noreply.github.com>
2021-05-10 15:53:53 +0100

    Update Peca
    criação variaveis

--- Projeto1/Peca.cs
+++ Projeto1/Peca.cs
@@ -1,5 +1,10 @@
 namespace Projeto1
 {
+
+    private int posX, posY;
+
+    private bool playerA; //se true então pertecen ao jogador A, se false pertence B
+
     public class Peca
     {
         
