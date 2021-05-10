697313349a10c6b364d84bf563bcd8adeb8788d3
Pedro Passos <73182976+pedro-22002182@users.noreply.github.com>
2021-05-10 16:25:39 +0100

    Update construtor Tabuleiro

--- Projeto1/Tabuleiro.cs
+++ Projeto1/Tabuleiro.cs
@@ -11,6 +11,19 @@ namespace Projeto1
             this.tabuleiro = new int [3][8];
             this.pecas = new Peca[14];
 
+            for(int i = 0; i < pecas.Length(); i++)
+            {
+                if(i <= 7)
+                {
+                    //criacao 7 pecas jogador A
+                    pecas[i] = new Peca(true);
+                }
+                else
+                {
+                    //criacao 7 pecas jogador B
+                    pecas[i] = new Peca(false);
+                }
+            }
         }
     }
 
