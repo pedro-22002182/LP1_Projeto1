0c948bbfbbcc13b6b92db0de9960e5163e3e42af
peta <coisasdohapo@gmail.com>
2021-05-12 15:43:59 +0100

    Criar game loop e lançar dados

--- Projeto1/Program.cs
+++ Projeto1/Program.cs
@@ -16,7 +16,40 @@ namespace Projeto1
 
             graficos.printMenu();
             graficos.showMap();
+
+            //Game loop
+            bool acabou = false;
+            
+            //Quando é true é o playerA a jogar e quando é false é o playerB
+            bool turno = true;
+
+            
+            
+
+            while (acabou == false)
+            {
+                graficos.showMap();
+                if(turno == true) 
+                    Console.WriteLine("É a vez do playerA");
+                else
+                    Console.WriteLine("É a vez do playerB");
+            }
             
         }
+
+        public int LançarDados()
+        {
+            Random numAleatorio = new Random();
+            int valorInteiro = numAleatorio.Next(0,5);
+
+            Console.WriteLine($" O numero é {valorInteiro}");
+            return valorInteiro;
+
+            
+
+        }
+
+
+
     }
 }
